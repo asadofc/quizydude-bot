d150f8af39302534094ef7e56036a312b611a441
Asad <mr.asadul.islam00@gmail.com>
2025-05-06 09:56:05 +0600

    Update quizydude.py

--- quizydude.py
+++ quizydude.py
@@ -20,11 +20,11 @@ logger = logging.getLogger(__name__)
 
 # --- DATABASE SETUP ---
 conn = psycopg2.connect(
-    dbname=os.environ.get('DB_NAME'),
-    user=os.environ.get('DB_USER'),
-    password=os.environ.get('DB_PASSWORD'),
-    host=os.environ.get('DB_HOST'),
-    port=os.environ.get('DB_PORT', 5432)
+    dbname=os.environ.get('PG_NAME'),
+    user=os.environ.get('PG_USER'),
+    password=os.environ.get('PG_PASSWORD'),
+    host=os.environ.get('PG_HOST'),
+    port=os.environ.get('PG_PORT', 5432)
 )
 cursor = conn.cursor()
 
